62c531c18a5f42f92802b9297b5cc48d9abde53b
SibonisoNdlovu <43060555+SibonisoNdlovu@users.noreply.github.com>
2019-05-07 10:04:56 +0200

    Add files via upload

---
+++ Greeting.Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Greeting.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting.Consume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOC.DependencyContainer.Initialize();
+            using (var messageService = IOC.DependencyContainer.Resolve<IMessageBus>())
+            {
+                Console.WriteLine("Listening for messages, press any key to close...");
+                messageService.GreetingChannel.Subscribe(model =>
+                {
+                    Console.WriteLine($"Hello {model.Name}, I am your father.");
+                });
+                Console.ReadKey();
+            }
+        }
+    }
+}
