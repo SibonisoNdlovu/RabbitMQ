66acf532a36be1e9947a3b41a9c4131474c3bea8
SibonisoNdlovu <43060555+SibonisoNdlovu@users.noreply.github.com>
2019-05-07 10:08:35 +0200

    Add files via upload

---
+++ Greeting.Producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using Greeting.Domain.Interfaces;
+using Greeting.Domain.Models;
+using Greeting.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOC.DependencyContainer.Initialize();
+            using (var messageBusService = IOC.DependencyContainer.Resolve<IMessageBus>())
+            {
+                var messageBusHandler = new MessageBusHandler(messageBusService.GreetingChannel);
+                Console.WriteLine("Please enter name");
+                var greeting = Console.ReadLine();
+                messageBusHandler.SendMessage(greeting);
+                Console.WriteLine("Message sent, press any key to close...");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class MessageBusHandler
+    {
+        private IMessageBusChannel<GreetingModel> _channel;
+        public MessageBusHandler(IMessageBusChannel<GreetingModel> channel)
+        {
+            _channel = channel;
+        }
+
+        public bool SendMessage(string name)
+        {
+            if (_channel.Publish(new GreetingModel { Name = name }))
+            {
+                return true;
+            }
+            throw new Exception("Could not send message");
+        }
+    }
+}
