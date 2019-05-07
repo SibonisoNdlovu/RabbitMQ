c9a384b5f4dcdc442162d768e357b850e2fd6d45
SibonisoNdlovu <43060555+SibonisoNdlovu@users.noreply.github.com>
2019-05-07 10:05:46 +0200

    Add files via upload

---
+++ Greeting.Domain/Interfaces/IMessageBusChannel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting.Domain.Interfaces
+{
+    public interface IMessageBusChannel<TMessage>
+    {
+        bool Publish(TMessage payLoad);
+
+        void Subscribe(Action<TMessage> action);
+    }
+}
