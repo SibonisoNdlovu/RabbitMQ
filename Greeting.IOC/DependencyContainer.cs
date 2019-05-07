e03b571bab8e48c3dfa24d64f159db344ac20ea3
SibonisoNdlovu <43060555+SibonisoNdlovu@users.noreply.github.com>
2019-05-07 10:06:52 +0200

    Add files via upload

---
+++ Greeting.IOC/DependencyContainer.cs
@@ -0,0 +1,31 @@
+﻿using Greeting.Domain.Interfaces;
+using Greeting.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace Greeting.IOC
+{
+    public class DependencyContainer
+    {
+        private static IUnityContainer _container;
+        public static void Initialize()
+        {
+            _container = new UnityContainer();
+            Setup();
+        }
+
+        public static T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        private static void Setup()
+        {
+            _container.RegisterType<IMessageBus, RabbitService>();
+        }
+    }
+}
